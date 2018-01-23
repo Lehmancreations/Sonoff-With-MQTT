d80ab83bd6e6e519a4b4999aad281c14ebc8685f
Andy Lehman <andy@lehmancreations.com>
2018-01-23 11:21:30 -0500

    Added link to the includes so they can be found on the net

--- Arduino/Sonoff-with-MQTT/Sonoff-with-MQTT.ino
+++ Arduino/Sonoff-with-MQTT/Sonoff-with-MQTT.ino
@@ -11,7 +11,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
-#include <PubSubClient.h>
+#include <PubSubClient.h> // https://github.com/knolleary/pubsubclient 
 #include <ArduinoOTA.h>
 
 /* 
