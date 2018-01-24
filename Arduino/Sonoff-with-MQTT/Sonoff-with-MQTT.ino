30dc51720f69b110b3ef38ab46175fb5ba2d42ea
Andy Lehman <andy@lehmancreations.com>
2018-01-24 14:53:17 -0500

    Initial WiFi manager try

--- Arduino/Sonoff-with-MQTT/Sonoff-with-MQTT.ino
+++ Arduino/Sonoff-with-MQTT/Sonoff-with-MQTT.ino
@@ -13,6 +13,13 @@
 #include <WiFiUdp.h>
 #include <PubSubClient.h> // https://github.com/knolleary/pubsubclient 
 #include <ArduinoOTA.h>
+// Needed for wifi manager
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+//for LED status
+#include <Ticker.h>
+Ticker ticker;
 
 // #define DHT22 // If defined then include and make use of the DHT22 Sensor 
 
@@ -64,6 +71,31 @@ byte button_pin = 0;
 WiFiClient wificlient;
 PubSubClient client(wificlient);
 
+
+/*
+ * The following is for the wifi manager 
+ */
+
+void tick()  //for boot up status
+{
+  //toggle state
+  int state = digitalRead(ledPin);  // get the current state of GPIO1 pin
+  digitalWrite(ledPin, !state);     // set pin to the opposite state
+}
+
+//gets called when WiFiManager enters configuration mode
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+  //entered config mode, make led toggle faster
+  ticker.attach(0.2, tick);
+}
+
+/*
+ * End Wifi manager set up
+ */
 
 /**
  * MQTT callback to process messages
@@ -143,18 +175,45 @@ void reconnect() {
  */
 void setup() {
   Serial.begin(115200);
-  Serial.println("Booting");
-  WiFi.mode(WIFI_STA);
-  WiFi.hostname("sonoff1");
-  WiFi.begin(ssid, password);
-  Serial.println("WiFi begun");
 
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
+  /* Set up the outputs. LED is active-low */
+  pinMode(ledPin, OUTPUT);
+  pinMode(relayPin, OUTPUT);
+  pinMode(button_pin, INPUT_PULLUP); // The input will be pulled LOW to trigger an action
+  digitalWrite(ledPin, HIGH);
+  digitalWrite(relayPin, LOW);
+  
+ // start ticker with 0.5 because we start in AP mode and try to connect
+  ticker.attach(0.6, tick);
+
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+  //reset settings - for testing
+  //wifiManager.resetSettings();
+
+  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+  wifiManager.setAPCallback(configModeCallback);
+
+WiFi.hostname("sonoff1");
+
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if (!wifiManager.autoConnect("Sonoff1")) {
+    Serial.println("failed to connect and hit timeout");
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(1000);
   }
-  Serial.println("Proceeding");
+
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+  ticker.detach();
+  //turn LED off
+  digitalWrite(BUILTIN_LED, HIGH);
+
 
   // Port defaults to 8266
   // ArduinoOTA.setPort(8266);
@@ -187,13 +246,7 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   
-  /* Set up the outputs. LED is active-low */
-  pinMode(ledPin, OUTPUT);
-  pinMode(relayPin, OUTPUT);
-  pinMode(button_pin, INPUT_PULLUP); // The input will be pulled LOW to trigger an action
-  digitalWrite(ledPin, HIGH);
-  digitalWrite(relayPin, LOW);
-  
+
 
   /* Prepare MQTT client */
   client.setServer(broker, 1883);
